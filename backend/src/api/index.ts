0a40ca2075624abc4379bf5b637599db75d9dd15
RaymartDev <raymart.sarmiento23@outlook.com>
2024-07-17 12:11:09 +0800

    Preparing for employee routes

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 
 import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+import employee from './employee';
 
 const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
+router.get<{}, MessageResponse>('/employee', (req, res) => {
   res.json({
     message: 'API - 👋🌎🌍🌏',
   });
 });
 
-router.use('/emojis', emojis);
+router.use('/employee', employee);
 
 export default router;
