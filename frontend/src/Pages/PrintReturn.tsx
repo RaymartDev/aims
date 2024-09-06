9ce24efad7cff2f1b4a7be757cb96b932d7f1b55
MicholoJB <buenafe.micholojoaquin.bscs2021@gmail.com>
2024-09-06 10:10:50 +0800

    print receipt temaplate

--- frontend/src/Pages/PrintReturn.tsx
+++ frontend/src/Pages/PrintReturn.tsx
@@ -5,36 +5,41 @@ function Downloadpdf() {
       name: "KINGSTON 64GB USB FLASH DRIVE",
       quantity: 4,
       unit: "PC",
-      status: "NA",
-      code: "OU",
+      serialNO: "1000232133",
+      remarks: "OU",
+      status: "status",
     },
     {
       name: "KINGSTON 64GB USB FLASH DRIVE",
       quantity: 4,
       unit: "PC",
-      status: "NA",
-      code: "OU",
+      serialNO: "1000232133",
+      remarks: "OU",
+      status: "status",
     },
     {
       name: "KINGSTON 64GB USB FLASH DRIVE",
       quantity: 4,
       unit: "PC",
-      status: "NA",
-      code: "OU",
+      serialNO: "1000232133",
+      remarks: "OU",
+      status: "status",
     },
     {
       name: "KINGSTON 64GB USB FLASH DRIVE",
       quantity: 4,
       unit: "PC",
-      status: "NA",
-      code: "OU",
+      serialNO: "1000232133",
+      remarks: "OU",
+      status: "status",
     },
     {
       name: "KINGSTON 64GB USB FLASH DRIVE",
       quantity: 4,
       unit: "PC",
-      status: "NA",
-      code: "OU",
+      serialNO: "1000232133",
+      remarks: "OU",
+      status: "status",
     },
   ];
 
@@ -42,11 +47,11 @@ function Downloadpdf() {
     ...items,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     ...Array(11 - items.length).fill({
-      name: "N/A",
+      name: "\u200B", // Zero-width space
       quantity: "",
       unit: "",
-      status: "",
-      code: "",
+      serialNO: "",
+      remarks: "",
     }),
   ];
 
@@ -54,23 +59,23 @@ function Downloadpdf() {
     <div className="w-full">
       <div className="flex justify-center mx-auto">
         <div className="w-full">
-          <div className="w-full justify-end flex px-6">
+          <div className="w-full justify-end flex px-6 mt-[-5px]">
             <p className="text-3xl font-bold">3289131</p>
           </div>
-          <div className="flex text-[10px] pl-20 pt-4 bg-red-100">
-            <p className="w-[325px] bg-blue-200">KFC Taguig Tagaytay Quezon</p>
+          <div className="flex text-[10px] pl-20 pt-4">
+            <p className="w-[325px]">KFC Taguig Tagaytay Quezon</p>
             <p className="w-[190px]">Company ni janloyd</p>
             <p className="w-[120px]">03/16/03</p>
             <p className="">17:02</p>
           </div>
-          <div className="flex text-[10px] pl-16 pt-3">
+          <div className="flex text-[10px] pl-20 pt-3">
             <p className="w-[800px]">
               Blk 6 lot 11 everlasting st. Dolmar Golden Hills
             </p>
-            <p>100232123</p>
+            <p>1400</p>
           </div>
-          <div className="pt-6 text-[8px]">
-            <table className="w-full table-auto border-none">
+          <div className="pt-7">
+            <table className="w-full table-auto pt-2">
               <tbody>
                 {filledItems.map((item, index) => (
                   <tr
@@ -79,65 +84,75 @@ function Downloadpdf() {
                       item.name === "N/A" ? "text-gray-400 text-[8px]" : ""
                     }
                   >
-                    <td className="border-none px-4 text-[8px]">
+                    <td className="px-2 text-[8px] w-[5%]">
                       {item.name !== "N/A" ? index + 1 : ""}
                     </td>
-                    <td className="border-none px-4 text-sm text-[8px]">
+                    <td className="pl-2 text-sm text-[7px] w-[30%]">
                       {item.name}
                     </td>
-                    <td className="border-none px-4 text-sm text-[8px]">
+                    <td className="pl-2 text-sm text-[7px]">
                       {item.quantity}
                     </td>
-                    <td className="border-none px-4 text-sm text-[8px]">
+                    <td className="text-sm text-[7px]">
                       {item.unit}
                     </td>
-                    <td className="border-none px-4 text-sm text-[8px]">
-                      {item.status}
+                    <td className="text-sm pl-4 text-[7px]">
+                      {item.serialNO}
                     </td>
-                    <td className="border-none px-4 text-sm text-[8px]">
-                      {item.code}
+                    <td className="pl-5 text-sm text-[7px]">
+                      {item.remarks}
+                    </td>
+                    <td className="text-sm text-[7px]">
+                      {item.status}
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-          <div className="flex gap-4 text-sm px-10">
-            <p>Guard Release:</p>
+          <div className="text-sm px-24 mt-[-8px] flex gap-32">
             <input
               type="text"
-              className="bg-transparent border-b border-black"
+              className="bg-transparent text-[12px]"
+            />
+            <input
+              type="text"
+              className="bg-transparent text-[12px]"
             />
           </div>
-          <div className="flex text-sm px-10 justify-between">
-            <div className="flex">
-              <div className="flex gap-4 text-sm">
-                <p>Time:</p>
-                <input
-                  type="text"
-                  className="bg-transparent border-b border-black w-24"
-                />
-              </div>
-              <div className="flex gap-4 text-sm">
-                <p>Date:</p>
-                <input
-                  type="text"
-                  className="bg-transparent border-b border-black w-24"
-                />
-              </div>
+          <div className="flex gap-16 pt-4">
+            <div className="ml-28">
+              <input
+                type="text"
+                className="bg-transparent text-[12px] w-[250px]"
+              />
             </div>
-            <div className="flex gap-4 text-sm">
-              <p className="text-sm">comment</p>
-              <p>DR#444527</p>
+            <div className="ml-[-110px]">
+              <input
+                type="text"
+                className="bg-transparent text-[12px]"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                className="bg-transparent text-[12px]"
+              />
             </div>
           </div>
-          <div className="px-10 pt-10 flex justify-between text-sm">
-            <p className="text-lg">ALFRED</p>
-            <p className="font-bold">LLOYD LEVIN REI S. PANGAN</p>
-          </div>
-          <div className="flex gap-4 px-10 pt-10 text-sm">
-            <p>2024-10-10</p>
-            <p>10:51</p>
+          <div className="flex justify-between pt-4">
+              <div className="flex pl-6 gap-8">
+                <input type="text" className="w-[75px] text-[12px] px-1"/>
+                <input type="text" className="w-[75px] text-[12px] px-2"/>
+              </div>
+              <div className="flex pl-20 gap-8">
+                <input type="text" className="w-[75px] text-[12px] px-1"/>
+                <input type="text" className="w-[75px] text-[12px] px-2"/>
+              </div>
+              <div className="flex pl-8 gap-8">
+                <input type="text" className="w-[75px] text-[12px] px-1"/>
+                <input type="text" className="w-[75px] text-[12px] px-2"/>
+              </div>
           </div>
         </div>
       </div>
