6e1a20f7656c515c5170ddd92bcdeef236b5af73
Leanseln <oleanselnico@gmail.com>
2024-10-08 13:59:13 +0800

    Shipped

--- frontend/src/modals/ShippedModal.tsx
+++ frontend/src/modals/ShippedModal.tsx
@@ -14,7 +14,7 @@ function ShippedModal({ open, onClose }: ShippedModalProps) {
         <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-20 p-4">
             <div className="flex flex-col w-1/2 2xl:w-1/4 bg-slate-50 rounded-2xl p-6">
                 <div className="flex items-center justify-between w-full border-b-2 border-black">
-                    <h1 className="font-extrabold text-xl">Shipped Item</h1>
+                    <h1 className="font-extrabold text-xl">Shipped By</h1>
                     <Button className="text-black bg-transparent hover:bg-transparent p-0" onClick={onClose}><X size={30}/></Button>
                 </div>
                 <div className="flex flex-col justify-start mt-5 space-y-2">
