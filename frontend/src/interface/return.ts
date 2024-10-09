d28e4057698032e431a6d29becb685d872d7af7a
Raymart <raymart.sarmiento23@outlook.com>
2024-10-10 07:24:26 +0800

    Updated

---
+++ frontend/src/interface/return.ts
@@ -0,0 +1,18 @@
+export default interface Return {
+    id: number;
+    return_number: number;
+    requestor: {
+      name: string;
+      employee_no: string;
+      cost_center_code: string;
+    };
+    details: {
+      detail_id: number;
+      material_id: number;
+      release_number: number;
+      desc: string;
+      quantity: number;
+      remarks: string;
+    }[];
+    tag: string;
+  }
