dc1ad6889a1224803deb4af415ff71a9b5202be9
Raymart <raymart.sarmiento23@outlook.com>
2024-10-09 17:56:05 +0800

    Updated

--- frontend/src/pages/Deliveries.tsx
+++ frontend/src/pages/Deliveries.tsx
@@ -402,7 +402,7 @@ function Deliveries() {
               <div className="relative w-1/3">
                 <Input
                   type="search"
-                  placeholder="Search Delivery Number"
+                  placeholder="Search Delivery Number / Product Description"
                   className="pl-12 border-2 focus:border-none"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
